4fa3a154b025dc53202a61e589d3ebfb5148048a
Heitor <heitorgomes900@gmail.com>
2018-08-29 22:57:15 -0300

    fundamentos de js

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,5 @@
+//comentario em linha
+/**
+ * comentarios de multiplas linhas
+ * 
+ */
---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,5 @@
+console.log(`sentenca de codigo`) //sentenca de codigo
+
+{
+    console.log('bloco de codigo') //bloco de codigo
+}
